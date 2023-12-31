9f1503815f167d39c2d86a2a4435c7f1b1c9516a
Pablo Goulart <pablogoulart02@gmail.com>
2023-12-31 14:09:44 -0300

    Mudança em loop por todas as imagens

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -16,12 +16,19 @@ function handleSlideshowButtonClick()
     const slideshowImage = getSlideshowImage();
     const currentSrc = slideshowImage.src;
 
-    if (currentSrc.includes("Dielman"))
+    if (currentSrc.includes("dielman"))
     {
-        slideshowImage.src = "img/green-ray.png"
+        slideshowImage.src = "img/green-ray.jpg"
     }
     else if (currentSrc.includes("ray"))
     {
-        slideshowImage.src = "img/Jeanne-Dielman.jpg"
+        slideshowImage.src = "img/twin-peaks.jpg"
+    }
+    else if (currentSrc.includes("peaks"))
+    {
+        slideshowImage.src = "img/mulholland-drive.jpg"
+    }
+    else{
+        slideshowImage.src = "img/jeanne-dielman.jpg"
     }
 }
