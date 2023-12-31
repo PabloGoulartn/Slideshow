c04a5a9390e52074479b8d5175b34a1cbb5407c6
Pablo Goulart <pablogoulart02@gmail.com>
2023-12-31 15:20:49 -0300

    Mudança no sistema de carrossel de imagem, usando for...in

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -11,24 +11,23 @@ document.querySelector('.prev-btn').addEventListener('click', function(){
     handleSlideshowButtonClick();
 });
 
-function handleSlideshowButtonClick()
-{
+function handleSlideshowButtonClick() {
     const slideshowImage = getSlideshowImage();
-    const currentSrc = slideshowImage.src;
 
-    if (currentSrc.includes("dielman"))
+    const imageMap = {
+        "dielman": "img/green-ray.jpg",
+        "ray": "img/twin-peaks.jpg",
+        "peaks": "img/mulholland-drive.jpg",
+        "drive": "img/jeanne-dielman.jpg"
+    };
+
+    for (const keyword in imageMap)
     {
-        slideshowImage.src = "img/green-ray.jpg"
-    }
-    else if (currentSrc.includes("ray"))
-    {
-        slideshowImage.src = "img/twin-peaks.jpg"
-    }
-    else if (currentSrc.includes("peaks"))
-    {
-        slideshowImage.src = "img/mulholland-drive.jpg"
-    }
-    else{
-        slideshowImage.src = "img/jeanne-dielman.jpg"
+        if(slideshowImage.src.includes(keyword))
+        {
+            slideshowImage.src = imageMap[keyword];
+
+            return;
+        }
     }
 }
